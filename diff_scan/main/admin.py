53d6d4c2f92986cd4985348014987904a2976283
chriscauley <chris@lablackey.com>
2016-02-27 21:46:18 -0500

    stashing changes to destroy droplet

--- diff_scan/main/admin.py
+++ diff_scan/main/admin.py
@@ -11,7 +11,7 @@ class PageAdmin(admin.ModelAdmin):
   readonly_fields = ('run_all_tests',)
   inlines = [PageTestInline]
   def run_all_tests(self,obj=None):
-    if not obj:
+    if not (obj and obj.pk):
       return "Please save before trying to run tests."
     return "<a href='%s'>Run all tests</a>"%(reverse("page_action",args=['test',obj.pk]))
   run_all_tests.allow_tags = True
